0ede514bc060beda59311e155556cb36f6875018
Jean Detoeuf <jdetoeuf.ext@orange.com>
2015-01-05 15:41:33 +0100

    Adding works if users have a space in their name
    Check if there are differences to commit
    Added gitignore

--- jenkins_scm.sh
+++ jenkins_scm.sh
@@ -1,27 +1,24 @@
 #!/bin/bash
 
-# Jenkins Configuraitons Directory
+# Jenkins Configurations Directory
 cd $JENKINS_HOME
-
 # Add general configurations, job configurations, and user content
 git add -- *.xml jobs/*/*.xml userContent/*
-
 # only add user configurations if they exist
 if [ -d users ]; then
-    user_configs=`ls users/*/config.xml`
-
-    if [ -n "$user_configs" ]; then
-        git add $user_configs
-    fi
+  user_configs=`ls users/*/config.xml`
+  if [ -n "$user_configs" ]; then
+    git add users/*/config.xml
+  fi
 fi
-
 # mark as deleted anything that's been, well, deleted
 to_remove=`git status | grep "deleted" | awk '{print $3}'`
-
 if [ -n "$to_remove" ]; then
-    git rm --ignore-unmatch $to_remove
+git rm --ignore-unmatch $to_remove
 fi
 
-git commit -m "Automated Jenkins commit"
-
-git push -q
+nb=$(git status -s | wc -l)
+if [ $nb -gt 0 ]; then
+  git commit -m "Automated Jenkins commit"
+  git push -q
+fi
